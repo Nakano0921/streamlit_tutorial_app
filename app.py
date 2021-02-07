2db39a07026c043fd57d61492ae484ba6368410c
Nakano0921 <ken.voyaga@icloud.com>
2021-02-08 02:08:47 +0900

    assesment.csvをgitignoreから外す

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.write('ファイルをアップロードすると評価一覧がみれます�
 uploaded_file = st.sidebar.file_uploader("ファイルをアップロード", type='csv')
 
 if uploaded_file != None:
-  st.header('「Ritz Carlton 水暉」の評価一覧')
+  st.header('「Ritz Carlton ロカンダ」の評価一覧')
   df = pd.read_csv(uploaded_file)
   df = df.drop(columns=df.columns[[0]])
   st.write(df)
@@ -36,9 +36,9 @@ if uploaded_file != None:
 
 
 
-compar_button = st.radio('平均点を比較する', ('和食', '12,000円〜15,000円', '京都'))
-if compar_button == '和食':
-  st.subheader('和食店の平均点は0.43点です')
+compar_button = st.radio('平均点を比較する', ('イタリアン', '12,000円〜15,000円', '京都'))
+if compar_button == 'イタリアン':
+  st.subheader('イタリアンの平均点は0.43点です')
 if compar_button == '12,000円〜15,000円':
   st.subheader('価格帯が12,000円から15,000円のお店の平均点は0.5点です')
 if compar_button == '京都':
